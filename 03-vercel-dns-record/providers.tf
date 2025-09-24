97e726fd1d1d6cb395345213cfd356097c24cf1d
omer <omer@levys.co.il>
2025-09-24 21:30:10 +0300

    vercel dns records

---
+++ 03-vercel-dns-record/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
